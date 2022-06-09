5c2796c08f36e8528714abf2e11ee4adf5217e9f
«kattsoy» <kathkondratenko@gmail.com>
2022-06-09 14:50:30 +0200

    Posts pagination

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,9 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('post/<int:pk>',PostDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>',PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
